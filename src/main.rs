96504b8b6c2cea19c2f4f12cee3001a044c23e9b
Roman <romanolsen941@gmail.com>
2023-05-31 19:23:23 -0600

    Update main.rs
    Cleaned up Formatting and deleted random comments

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,15 @@ use image::{open};
 use image::{DynamicImage,Pixel};
 use clap::Parser;
 
-
-
-
-
-
-
-
 fn main(){
-
-
     #[derive(Parser, Debug)]
     #[command(author, version, about, long_about = None)]
     struct Args {
-        /// Name of the person to greet
+        /// Path of Image
         #[arg(short, long)]
         path: String,
 
-        /// Number of times to greet
+        /// Width of Ascii art in characters
         #[arg(short, long, default_value_t = 32)]
         resolution: u32,
     }
@@ -30,10 +21,6 @@ fn main(){
 
     let LUMINANCE_SCALE_SMALL: String = String::from(".,-~:;=!*#$@");
     let STEP_SMALL: u8 = (255/LUMINANCE_SCALE_SMALL.len()).try_into().unwrap();
-    
-//".'`^\",:;Il!i<>~+_-?[]{}1()|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
-
-
 
     let rgba = open(args.path).unwrap().into_rgba8();
     let gray = DynamicImage::ImageRgba8(rgba).into_luma8();
@@ -53,7 +40,6 @@ fn main(){
                 println!("{},{}",brightness,STEP_SMALL);
                 '.'
             }));
-            //ascii_buf.push(' ');
         }
         ascii_buf.push('\n');
     }
@@ -61,36 +47,3 @@ fn main(){
     println!("{}",ascii_buf);
     gray.save("grey.png");
 }
-
-
-/*           UCRAZY.PNG
-    let on_top = open("kot.png").unwrap().into_rgb8();
-    let mut img = ImageBuffer::from_fn(1024, 1024, |x, y| {
-        if (x + y) % 2 == 0 {
-            let r = (x/5) as u8;
-            
-            image::Rgb([r,0,0])
-        } else {
-            let r = (x/5) as u8;
-            image::Rgb([255,10,255])
-        }
-    });
-
-    image::imageops::overlay(&mut img, &on_top, 128, 128);
-
-    img.save("ucrazy.png");
-*/
-
-/*            EMPTY,JPG
-let mut image = RgbImage::new(64,64);
-
-    for x in 0..64{
-        for y in 0..64{
-            let g:u8 = 2*x;
-            let b:u8 = 3*y;
-            image.put_pixel(x.into(),y.into(),Rgb([100,g,b]));
-        }
-    }
-
-    image.save("empty.jpg");
-*/
