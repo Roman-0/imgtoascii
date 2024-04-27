1df96d728da0c6d256f60ee94f58aa9fa1bfe509
Roman-0 <romanolsen941@gmail.com>
2024-04-27 01:54:35 -0600

    Changed the main file and added new images

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use image::open;
-use image::{DynamicImage, Pixel};
+use image::Pixel;
 
 fn main() {
     #[derive(Parser, Debug)]
@@ -16,23 +16,26 @@ fn main() {
     }
     let args = Args::parse();
 
-    let ascii_resolution_y: u32 = args.resolution * 2;
-    let ascii_resolution_x: u32 = args.resolution;
+    let ascii_resolution_y: u32 = args.resolution; //32
+    let ascii_resolution_x: u32 = args.resolution*2; //32
 
-    let luminance_scale_small: String = String::from(".,-~:;=!*#$@");
-    let step_small: u8 = (255 / luminance_scale_small.len()).try_into().unwrap();
+    let luminance_scale: String = String::from(".,-~:;=!*#$@");
+    let step_small: u8 = 21; //(255 / luminance_scale_small.len()).try_into().unwrap();
 
-    let rgba = open(args.path).unwrap().into_rgba8();
-    let gray = DynamicImage::ImageRgba8(rgba).into_luma8();
+    let grey = open(args.path).unwrap_or_else(|err| {
+        eprintln!("Error opening image. {}",err);
+        std::process::exit(1);
+    }).into_luma8();
 
-    let image_height = gray.height();
-    let image_width = gray.width();
+    let image_height = grey.height(); //2353
+    let image_width = grey.width(); //2000
+
     let mut ascii_buf = String::new();
 
-    for row in 0..ascii_resolution_x {
-        for col in 0..ascii_resolution_y {
-            let y = image_width / (ascii_resolution_x + 1) * (row + 1);
-            let x = image_height / (ascii_resolution_y + 1) * (col + 1);
+    /*for row in 0..ascii_resolution_x{
+        for col in 0..ascii_resolution_y{
+            let y:u32 = (image_height / (ascii_resolution_y + 1) * (col + 1)).into();
+            let x:u32 = (image_width / (ascii_resolution_x + 1) * (row + 1)).into();
 
             //TAKE AVERAGE PIXEL VALUE
             let pixel = gray.get_pixel(x, y);
@@ -46,7 +49,29 @@ fn main() {
             }));
         }
         ascii_buf.push('\n');
-    }
+    }*/
+    let x_step: u32 = image_width / ascii_resolution_x;
+    let y_step: u32 = image_height / ascii_resolution_y;
+    for col in 1..ascii_resolution_y{
+        for row in 1..ascii_resolution_x{
+            let x: u32 = x_step * row;
+            let y: u32 = y_step * col;
 
+            let brightness = grey.get_pixel(x, y).channels()[0];
+            let character = luminance_scale.chars().nth((brightness/step_small).into());
+
+            ascii_buf.push(character.unwrap_or_else(|| {
+                println!("{},{}", brightness, step_small);
+                ' '
+            }));
+            
+        }
+        ascii_buf.push('\n');
+    }
     println!("{}", ascii_buf);
 }
+/*
+fn get_avg_pixel_brightness(x: u32, y: u32) -> u32 {
+
+}
+*/
